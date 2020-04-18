25e0fe16962b8260511b1bfdaf35413ae0e75051
Ron <srl@gapps.knu.edu.tw>
2020-04-18 19:55:31 +0800

    msys2

--- 0_gcp_creds.sh
+++ 0_gcp_creds.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# source ./p0_source_creds.sh
-export GCP_PROJECT='ansible-gce-demo'
+# source ./0_source_creds.sh
+export GCP_PROJECT='aiot-249002'
 export GCP_AUTH_KIND='serviceaccount'
 # export GCP_SERVICE_ACCOUNT_EMAIL='xxxxxx.iam.gserviceaccount.com'
-export GCP_SERVICE_ACCOUNT_FILE='~/Documents/Personal/gcp_creds/ansible-gce-demo-a0dbb4ac2ff7.json'
+export GCP_SERVICE_ACCOUNT_FILE='~/aiot-249002-5b4370b0991c.json'
 export GCP_SCOPES='https://www.googleapis.com/auth/compute'
+
