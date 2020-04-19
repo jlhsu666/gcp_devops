e2f79dd89c25669a7f63a513c3a9e44c11712143
Ron <srl@gapps.knu.edu.tw>
2020-04-19 17:26:28 +0800

    revised

--- 0_gcp_creds.sh
+++ 0_gcp_creds.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # source ./0_source_creds.sh
-export GCP_PROJECT='aiot-249002'
+export GCP_PROJECT='project-id-8122930850481580817'
 export GCP_AUTH_KIND='serviceaccount'
-# export GCP_SERVICE_ACCOUNT_EMAIL='xxxxxx.iam.gserviceaccount.com'
-export GCP_SERVICE_ACCOUNT_FILE='~/aiot-249002-5b4370b0991c.json'
+export GCP_SERVICE_ACCOUNT_FILE='~/project-id-8122930850481580817-80947e524565.json'
 export GCP_SCOPES='https://www.googleapis.com/auth/compute'
-
+export ANSIBLE_SSH_ARGS='-o ControlMaster=no'
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 ReadMe
+
+Usage:
+
+Change the variables in the following files:
+	1. ansible.cfg  
+	2. 0_gcp_creds.sh  
+	3. inventories/webservers_gcp.yml
+
+source ./0_gcp_creds.sh
+
+ansible-playbook -t create playbooks/10_webserver_infra.yml
+
+ansible-playbook -i inventories/webservers.gcp.yml -u ansible playbooks/20_webserver_config.yml
+
+ansible-playbook -t delete playbooks/10_webserver_infra.yml
+
